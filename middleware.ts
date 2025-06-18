2a194602a238779ab58c04fb52590b8e368071fa
otw7917 <otw7917@gmail.com>
2025-06-18 11:51:28 +0900

    feat: login page component with supabaseClient using google Oauth

--- middleware.ts
+++ middleware.ts
@@ -9,11 +9,14 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
+     * - /
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - image extensions like .svg, .png, .jpg, etc.
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+
+    "/((?!^/$|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -39,10 +39,10 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser();
 
+  const PUBLIC_PATH = ["/", "/login", "/auth"];
   if (
     !user &&
-    !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth")
+    !PUBLIC_PATH.some((path) => request.nextUrl.pathname.startsWith(path))
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
